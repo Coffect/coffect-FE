04194c20a4a0a523a0f20f600b46013630601db5
jeha0714 <whrydus0714@gmail.com>
2025-07-04 08:58:39 +0900

    feat: Add Mypage router that related url

---
+++ coffect/src/routes/userPageRoutes.tsx
@@ -0,0 +1,43 @@
+/*
+author : 재하
+description : 마이페이지, 유저페이지 등 사용자 관련 라우트 정의 파일입니다.
+*/
+
+import ChatCard from "../components/UserPage/MyPage/ChatRecord/ChatCard";
+import ChatRecord from "../components/UserPage/MyPage/ChatRecord/ChatRecord";
+import Leave from "../components/UserPage/MyPage/Leave/Leave";
+import MyPage from "../components/UserPage/MyPage/MyPage";
+import TimeTable from "../components/UserPage/MyPage/TimeTable";
+import UserPage from "../pages/UserPage";
+
+/*
+사용자 관련 페이지 라우트 배열을 반환합니다.
+*/
+const userPageRoutes = [
+  {
+    path: "/mypage",
+    element: <MyPage />,
+  },
+  {
+    path: "/mypage/timetable",
+    element: <TimeTable />,
+  },
+  {
+    path: "/mypage/leave",
+    element: <Leave />,
+  },
+  {
+    path: "/mypage/chatrecord",
+    element: <ChatRecord />,
+  },
+  {
+    path: "/mypage/chatrecord/:id",
+    element: <ChatCard />,
+  },
+  {
+    path: "/userpage/:id",
+    element: <UserPage />,
+  },
+];
+
+export default userPageRoutes;
