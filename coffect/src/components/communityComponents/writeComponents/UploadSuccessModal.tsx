3804aa3812b516dc4e168f6511d0093b07829282
shinwokkang <tlsdnr303@gmail.com>
2025-07-09 11:11:02 +0900

    feat: 글 작성 완료버튼 클릭 시 나오는 모달 UI 구현

--- coffect/src/components/communityComponents/writeComponents/UploadSuccessModal.tsx
+++ coffect/src/components/communityComponents/writeComponents/UploadSuccessModal.tsx
@@ -1,42 +1,42 @@
-/* author : 강신욱
+/* 
+author : 강신욱
 description : 글 업로드 성공 시 나타나는 모달 컴포넌트입니다.
 */
 import React from "react";
+import UploadSuccessModalImage from "../../../assets/images/UploadSuccessModalImage.png";
 
 interface UploadSuccessModalProps {
   isOpen: boolean;
   onViewPost: () => void;
-  onGoHome: () => void;
 }
 
 const UploadSuccessModal: React.FC<UploadSuccessModalProps> = ({
   isOpen,
   onViewPost,
-  onGoHome,
 }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="bg-opacity-10 fixed inset-0 z-50 flex w-full items-center justify-center backdrop-brightness-30">
-      <div className="flex w-80 flex-col items-center gap-5 rounded-2xl bg-white p-6 shadow-lg">
-        <h2 className="text-lg font-bold">글이 게시되었어요.</h2>
-        <div className="mb-4 text-center text-sm text-gray-500">
-          나의 가치관과 생각을 들어내는 글은
-          <br />더 많은 유저들에게 자기 어필을 할 수 있어요!
-        </div>
-
-        <div className="fled-col flex h-12 w-full gap-3">
+    <div className="fixed inset-0 z-50 flex justify-center">
+      <div className="bg-opacity-50 pointer-events-auto flex h-full w-full max-w-[430px] items-center justify-center backdrop-brightness-50">
+        <div className="relative flex w-[72.8%] flex-col items-center overflow-hidden rounded-[20px] bg-white shadow-lg">
+          <div className="flex flex-col items-center gap-2 pr-8 pl-8">
+            <h2 className="pt-10 text-lg font-bold">글이 게시되었어요!</h2>
+            <div className="text-center text-sm text-gray-500">
+              나의 가치관과 생각을 들어내는 글은 더 많은 유저들에게 자기 어필을
+              할 수 있어요!
+            </div>
+            <img
+              src={UploadSuccessModalImage}
+              alt="업로드 성공"
+              className="w-[50%]"
+            />
+          </div>
           <button
             onClick={onViewPost}
-            className="w-full bg-black py-3 font-semibold text-white"
+            className="w-full bg-[#2d2d2d] py-5 font-semibold text-white"
           >
-            내 글 확인하기
-          </button>
-          <button
-            onClick={onGoHome}
-            className="w-full border border-gray-300 bg-white py-3 font-semibold text-gray-500"
-          >
-            홈으로
+            확인하기
           </button>
         </div>
       </div>
