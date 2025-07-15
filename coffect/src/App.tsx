a33ae1630c9a990fa71ad19b5c966fdf7c43fb38
shinwokkang <tlsdnr303@gmail.com>
2025-07-15 21:55:57 +0900

    Merge branch 'feature/community' of https://github.com/Coffect/coffect-FE into feature/community

--- coffect/src/App.tsx
+++ coffect/src/App.tsx
@@ -1,13 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/layout/RootLayout";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import CommunityRoutes from "./routes/CommunityRoutes";
+import Community from "./pages/Community";
+import Chat from "./pages/Chat";
 import ChatRouter from "./routes/ChatRouter";
 import userPageRoutes from "./routes/userPageRoutes";
 import homePageRoutes from "./routes/homePageRoutes";
 import signupRoutes from "./routes/signupRoutes";
+import CommunityRoutes from "./routes/CommunityRoutes";
 
 const router = createBrowserRouter([
   {
@@ -19,17 +22,29 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
+        path: "chat",
+        element: <Chat />,
+      },
+      {
         path: "signup",
         element: <Signup />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "/community",
+        element: <Community />,
       },
       {
         path: "/chat/*",
         element: <ChatRouter />,
       },
-      ...CommunityRoutes,
       ...userPageRoutes,
       ...homePageRoutes,
       ...signupRoutes,
+      ...CommunityRoutes,
     ],
   },
 ]);
