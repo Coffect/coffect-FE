6a544492770ec140d11b78a492163f53c1803632
shinwokkang <tlsdnr303@gmail.com>
2025-07-16 10:20:38 +0900

    fix: 320에서 글 작성 시 가로 스크롤 생기는 버그 수정 (ChipGroup 의 w-[318px] -> w-full )

--- coffect/src/components/communityComponents/common/ChipGroup.tsx
+++ coffect/src/components/communityComponents/common/ChipGroup.tsx
@@ -28,7 +28,7 @@ const ChipGroup: React.FC<ChipGroupProps> = ({
   const sliceEnd = title === "글 주제 선택" ? 4 : 3;
   return (
     <div className="mb-4">
-      <h2 className="mb-2 h-[27px] w-[318px] text-left font-bold">{title}</h2>
+      <h2 className="mb-2 h-[27px] w-full text-left font-bold">{title}</h2>
       <div className="flex flex-col gap-2">
         <div className="flex flex-wrap justify-start gap-2">
           {options.slice(0, sliceEnd).map((option) => (
