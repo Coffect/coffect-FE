7cfdec4cfe10329d731769f1fdfb44fbbf7a51f6
shinwokkang <tlsdnr303@gmail.com>
2025-07-20 21:16:25 +0900

    fix: 댓글 사진과 댓글 사이 간격 수정 및 가운데로 수정

--- coffect/src/components/communityComponents/comment/CommentList.tsx
+++ coffect/src/components/communityComponents/comment/CommentList.tsx
@@ -11,19 +11,26 @@ interface CommentListProps {
 
 const CommentList = ({ commentList }: CommentListProps) => {
   return (
-    <div className="flex-grow overflow-y-auto p-2">
-      <p className="mb-4 font-semibold">댓글 {commentList.length}개</p>
-      <div className="space-y-4">
+    <div className="flex-grow overflow-y-auto">
+      <div className="mb-4 font-semibold">댓글 {commentList.length}개</div>
+      <div className="">
         {commentList.map((comment) => (
-          <div key={comment.id} className="flex items-start gap-3">
+          <div key={comment.id} className="flex items-start gap-5 py-2.5">
             <img
               src={comment.user.avatar}
               alt={comment.user.name}
-              className="h-10 w-10 rounded-full"
+              className="h-10 w-10 rounded-full object-cover"
             />
-            <div className="flex-grow">
-              <p className="font-semibold">{comment.user.name}</p>
-              <p className="text-gray-700">{comment.content}</p>
+            <div className="flex flex-grow flex-col justify-center gap-1.5">
+              <div className="flex items-center gap-2">
+                <span className="font-semibold text-[var(--gray-90)]">
+                  양관식
+                </span>
+                <span className="text-sm text-[var(--gray-40)]">
+                  경영학과 23학번
+                </span>
+              </div>
+              <p className="font-md text-[var(--gray-90)]">{comment.content}</p>
             </div>
           </div>
         ))}
