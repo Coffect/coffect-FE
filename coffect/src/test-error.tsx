2c09a6fdc7f32b7b0b3f9125c3cfec3486f06e46
amu601@naver.com <amu601@naver.com>
2025-07-24 09:54:29 +0900

    test: ESLint error test

---
+++ coffect/src/test-error.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TestErrorComponent = () => {
+  const unusedVariable = "test"; // ESLint 오류: 사용하지 않는 변수
+  const badFormatting = 123; // Prettier 오류: 공백 없음
+
+  return (
+    <div>
+      <h1>Test Error Component</h1>
+    </div>
+  );
+};
+
+export default TestErrorComponent;
