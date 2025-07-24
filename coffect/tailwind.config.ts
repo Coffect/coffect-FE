a51c943c3b17c55f8c94d118b6bc8891a6b9b2f0
amu601@naver.com <amu601@naver.com>
2025-07-24 11:00:00 +0900

    refactor: reset.css를 없애고 tailwind 기본 적용 레이어로 통합

--- coffect/tailwind.config.ts
+++ coffect/tailwind.config.ts
@@ -1,4 +1,7 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+
+const config: Config = {
   theme: {
     extend: {
       fontFamily: {
@@ -21,4 +24,53 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        "html, body, div, span, applet, object, iframe,\
+        h1, h2, h3, h4, h5, h6, p, blockquote, pre,\
+        a, abbr, acronym, address, big, cite, code,\
+        del, dfn, em, img, ins, kbd, q, s, samp,\
+        small, strike, strong, sub, sup, tt, var,\
+        b, u, i, center,\
+        dl, dt, dd, ol, ul, li,\
+        fieldset, form, label, legend,\
+        table, caption, tbody, tfoot, thead, tr, th, td,\
+        main, article, aside, canvas, details, embed,\
+        figure, figcaption, footer, header, hgroup,\
+        menu, nav, output, ruby, section, summary,\
+        time, mark, audio, video, input, button": {
+          margin: "0",
+          padding: "0",
+          border: "0",
+          fontSize: "100%",
+          font: "inherit",
+          verticalAlign: "baseline",
+        },
+        "article, aside, details, figcaption, figure,\
+        footer, header, hgroup, menu, nav, section, div": {
+          display: "block",
+        },
+        body: {
+          lineHeight: "1",
+        },
+        "ol, ul": {
+          listStyle: "none",
+        },
+        "blockquote, q": {
+          quotes: "none",
+        },
+        "blockquote:before, blockquote:after,\
+        q:before, q:after": {
+          content: "''",
+        },
+        table: {
+          borderCollapse: "collapse",
+          borderSpacing: "0",
+        },
+      });
+    }),
+  ],
 };
+
+export default config;
