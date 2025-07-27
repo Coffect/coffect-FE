575ee2c0acf7e05ac41e1dc8f967ae4e50ea723f
shinwokkang <tlsdnr303@gmail.com>
2025-07-27 15:41:26 +0900

    refactor: useComments는 postId를 인자로 받으므로, usePostDetail.ts 의 useComment에 postId 를 주도록 수정

--- coffect/src/hooks/usePostDetail.ts
+++ coffect/src/hooks/usePostDetail.ts
@@ -1,9 +1,8 @@
-/*
-author : 강신욱
-description : 게시글 상세 페이지의 데이터 로직을 관리하는 커스텀 훅입니다.
-              URL에서 게시글 ID를 가져와 올바른 게시글 데이터를 찾고, 
-              해당 게시글의 댓글 목록을 불러오는 등 모든 데이터 관련 로직을 처리합니다.
-*/
+/**
+ * @author: 강신욱
+ * @description: 게시글 상세 페이지 데이터 로직을 관리하는 커스텀 훅
+ *
+ */
 import { useParams } from "react-router-dom";
 import { generateDummyPosts } from "../data/communityDummyData";
 import { useComments } from "./useComments";
@@ -21,18 +20,15 @@ const getTimeAgo = (date: Date): string => {
 
 // 게시글 상세 페이지의 데이터를 가져오는 함수
 export const usePostDetail = () => {
-  // URL에서 게시글 ID를 가져옵니다.
   const { id } = useParams<{ id: string }>();
   const postId = Number(id);
 
-  // 모든 게시글 데이터를 가져옵니다.
   const allPosts = generateDummyPosts({ type: null, topic: null });
   const post = allPosts.find((p) => p.id === postId);
 
-  // 댓글 목록을 가져옵니다.
-  const { comments: commentList } = useComments();
+  // postId를 useComments 훅에 전달합니다.
+  const { comments: commentList } = useComments(postId);
 
-  // 게시글이 존재하지 않을 경우 빈 객체를 반환합니다.
   const timeAgo = post ? getTimeAgo(post.postedDate) : "";
 
   return {
