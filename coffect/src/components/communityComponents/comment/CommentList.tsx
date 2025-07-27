884429e362b3404ac2503acf92a1932a9a79cc9b
shinwokkang <tlsdnr303@gmail.com>
2025-07-27 15:36:08 +0900

    refactor: CommentItem.tsx를 받아 map 함수를 활용하여 UI에 렌더링 하는 역할만 하도록 수정

--- coffect/src/components/communityComponents/comment/CommentList.tsx
+++ coffect/src/components/communityComponents/comment/CommentList.tsx
@@ -1,10 +1,15 @@
-/*
-author: 강신욱
-description: 댓글 목록을 표시하는 컴포넌트입니다.
+/**
+ * @author: 흥부/강신욱
+ * @description:  CommentItem에 map 함수를 사용하여 댓글 목록을 표시하는 컴포넌트
  */
+import type { Comment } from "../../../types/commentTypes";
+import CommentItem from "./CommentItem";
 
-import type { Comment } from "../../../hooks/useComments";
-
+/**
+ * @interface CommentListProps
+ * @description: CommentList 컴포넌트에 전달되는 props 타입 정의
+ * @property {Comment[]} commentList - 댓글 데이터 배열
+ */
 interface CommentListProps {
   commentList: Comment[];
 }
@@ -15,24 +20,7 @@ const CommentList = ({ commentList }: CommentListProps) => {
       <div className="mb-4 font-semibold">댓글 {commentList.length}개</div>
       <div className="">
         {commentList.map((comment) => (
-          <div key={comment.id} className="flex items-start gap-5 py-2.5">
-            <img
-              src={comment.user.avatar}
-              alt={comment.user.name}
-              className="h-10 w-10 rounded-full object-cover"
-            />
-            <div className="flex flex-grow flex-col justify-center gap-1.5">
-              <div className="flex items-center gap-2">
-                <span className="font-semibold text-[var(--gray-90)]">
-                  양관식
-                </span>
-                <span className="text-sm text-[var(--gray-40)]">
-                  경영학과 23학번
-                </span>
-              </div>
-              <p className="font-md text-[var(--gray-90)]">{comment.content}</p>
-            </div>
-          </div>
+          <CommentItem key={comment.id} comment={comment} />
         ))}
       </div>
     </div>
