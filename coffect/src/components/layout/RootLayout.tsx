b615c816849549dde9dc91b0bb828e44407c4545
jeha0714 <whrydus0714@gmail.com>
2025-08-15 02:44:35 +0900

    bug fix

--- coffect/src/components/layout/RootLayout.tsx
+++ coffect/src/components/layout/RootLayout.tsx
@@ -10,7 +10,7 @@ const RootLayout = () => {
     <div className="flex h-dvh w-dvw justify-center bg-gray-100">
       <div className="flex h-full w-full max-w-[430px] flex-col bg-white">
         {/* Main 영역 */}
-        <main className="h-full w-full bg-green-100">
+        <main className="h-full w-full">
           <Outlet />
         </main>
       </div>
