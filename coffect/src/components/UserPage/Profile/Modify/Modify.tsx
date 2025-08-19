c7f6dbe24d63cd1cbd689857148bab8b7f868108
jeha0714 <whrydus0714@gmail.com>
2025-08-19 14:01:21 +0900

    fix: revise navigate specific path to -1

--- coffect/src/components/UserPage/Profile/Modify/Modify.tsx
+++ coffect/src/components/UserPage/Profile/Modify/Modify.tsx
@@ -228,7 +228,7 @@ const Modify = () => {
       <div className="flex items-center justify-between py-3">
         <button
           className="pr-9 text-left text-3xl"
-          onClick={() => navigate("/mypage/myprofile")}
+          onClick={() => navigate(-1)}
         >
           <img src={backIcon} className="h-6 w-6" />
         </button>
